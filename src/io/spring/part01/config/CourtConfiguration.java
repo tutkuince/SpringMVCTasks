e70bc0122607074105f3c605952f0ef1bafd8d7d
tutkuince <tutku.ince@outlook.com>
2018-07-20 17:15:54 +0300

    CourtConfig class

---
+++ src/io/spring/part01/config/CourtConfiguration.java
@@ -0,0 +1,21 @@
+package io.spring.part01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("io.spring.part01")
+public class CourtConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/part01/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
