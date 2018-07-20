08e9d0e2443bea42568c6f989df0801a2f46a112
tutkuince <tutku.ince@outlook.com>
2018-07-20 18:01:21 +0300

    Member class

---
+++ src/io/spring/part02/domain/Member.java
@@ -0,0 +1,14 @@
+package io.spring.part02.domain;
+
+public class Member {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
