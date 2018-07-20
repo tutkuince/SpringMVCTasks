20a5b0dd997e8bad1a45037d4f38ed1da08a195c
tutkuince <tutku.ince@outlook.com>
2018-07-20 15:35:57 +0300

    created Player domain class

---
+++ src/io/spring/part01/domain/Player.java
@@ -0,0 +1,33 @@
+package io.spring.part01.domain;
+
+public class Player {
+	private String name;
+	private String phone;
+
+	public Player() {
+
+	}
+
+	public Player(String name, String phone) {
+		super();
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
