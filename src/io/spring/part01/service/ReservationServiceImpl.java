0ca6f61cc9631a10da1329eb5d5ca19552f99f99
tutkuince <tutku.ince@outlook.com>
2018-07-20 15:37:07 +0300

    created service implementation class

---
+++ src/io/spring/part01/service/ReservationServiceImpl.java
@@ -0,0 +1,29 @@
+package io.spring.part01.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import io.spring.part01.domain.Player;
+import io.spring.part01.domain.Reservation;
+import io.spring.part01.domain.SportType;
+
+public class ReservationServiceImpl implements ReservationService {
+	public static final SportType TENNIS = new SportType(1, "Tennis");
+	public static final SportType SOCCER = new SportType(2, "Soccer");
+
+	private final List<Reservation> reservations = new ArrayList<>();
+
+	public ReservationServiceImpl() {
+		reservations.add(new Reservation("Tennis #1", LocalDate.now(), 16, new Player("Tutku", "N/A"), TENNIS));
+		reservations.add(new Reservation("Tennis #2", LocalDate.now(), 20, new Player("Emin", "N/A"), TENNIS));
+	}
+
+	@Override
+	public List<Reservation> query(String courtName) {
+		return this.reservations.stream().filter(reservation -> Objects.equals(reservation.getCourtName(), courtName))
+				.collect(Collectors.toList());
+	}
+}
